7f3f0b86e03f5c75c19e70cea9e5cdaa35921b94
Fernando Morais <fernando.morais@devscope.net>
2017-03-08 16:01:26 +0000

    Add: Test Project and Nuget related plumbing

---
+++ EzAPITest/EzAPIText.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.SqlServer.SSIS.EzAPI;
+using Microsoft.SqlServer.Dts.Runtime;
+using System.Text;
+using System.IO;
+
+namespace EzAPITest
+{
+    [TestClass]
+    public class EzAPIText
+    {
+        [TestMethod]
+        public void CheckNoFailLoadFromExistingPackage()
+        {
+            EzPackage package = new EzPackage();
+            package.LoadFromFile(@"D:\Projectos\DevScope\EzApi\BI_adir_AdmissaoDirecta.dtsx");
+
+            Assert.AreEqual(package.Name, "BI_adir_AdmissaoDirecta");
+        }
+
+        [TestMethod]
+        public void CheckListOfVariables()
+        {
+            EzPackage package = new EzPackage();
+            package.LoadFromFile(@"D:\Projectos\DevScope\EzApi\BI_adir_AdmissaoDirecta.dtsx");
+            package.SaveToFile(@"D:\Projectos\DevScope\EzApi\BI_adir_AdmissaoDirectaGerado.dtsx");
+
+            EzPackage package1 = new EzPackage();
+            package1.LoadFromFile(@"D:\Projectos\DevScope\EzApi\BI_adir_AdmissaoDirectaGerado.dtsx");
+
+            Variables variables = package1.Variables;
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Name;Description");
+
+            foreach (Variable x in variables)
+            {
+                sb.AppendLine(x.Name + ";" + x.Description + ";" + x.Value.ToString());
+            }
+
+            File.WriteAllText(@"D:\Projectos\DevScope\EzApi\Variables.csv", sb.ToString());
+
+            Assert.AreEqual("", "");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EzApi")]
+[assembly: AssemblyDescription("Fork of EzApi http://sqlsrvintegrationsrv.codeplex.com/releases/view/21238 adapted for SQL Server 2016")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EzApi")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e4d7f98-490d-450b-887a-690918d9e57c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyFileVersion("0.8.*.*")]
